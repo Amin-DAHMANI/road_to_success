f8bef626d5b5921a3c952a3809c92b372eacfda3
Amîn DAHMANI <amin.dhm.otm.pro@gmail.com>
2023-07-26 17:54:26 +0200

    Test again

--- frontend/src/components/Pages/Registration/Registration.js
+++ frontend/src/components/Pages/Registration/Registration.js
@@ -35,7 +35,7 @@ function Registration() {
       emailRegistration
     );
     e.preventDefault();
-    await axios({
+    axios({
       method: "post",
       url: `${process.env.REACT_APP_API_URL}api/user/inscription`,
       withCredentials: true,
